13b505f9384c33e3785f778b74882574a7d3235c
abdullah-al-monir <abdullahalmonir364@gmail.com>
2025-01-04 02:25:05 +0600

    Added a file where defines single line comment and multiple line comment

---
+++ CommentDemo.java
@@ -0,0 +1,10 @@
+public class CommentDemo {
+    public static void main(String[] args) {
+        System.out.println("Monir"); //printing my name
+
+        /*
+        Multiple Line comment
+        My name is Abdullah Al Monir.
+         */
+    }
+}
--- First.java
+++ First.java
@@ -1,5 +1,8 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("I will be a perfect guy in Java Oneday!");
+        System.out.println("Abdullah Al Monir \nSoftware Developer \nSynchronise IT");
+        System.out.println("1 \t 2");
+        System.out.println("3 \t 4");
+        System.out.println("\"We don't talk anymore...\"");
     }
 }
